611d94eabfaf127eb8d7ab32aa817b0cde211c44
ZxBing0066 <ZxBing0066@gmail.com>
2021-10-26 11:09:50 +0800

    add build & ignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [reactRefresh()],
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'src/App.tsx'),
+            name: 'design-token-editor',
+            fileName: (format) => `design-token-editor.${format}.js`,
+        },
+        rollupOptions: {
+            // make sure to externalize deps that shouldn't be bundled
+            // into your library
+            external: ['react'],
+            output: {
+                // Provide global variables to use in the UMD build
+                // for externalized deps
+                globals: {
+                    react: 'React',
+                },
+            },
+        },
+    },
 });
